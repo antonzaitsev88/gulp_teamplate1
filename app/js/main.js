8c5af8667d970ff0c528a62ce38fac5dd030d4b8
anton_zaitsev <anton_zayzev@mail.ru>
2020-04-12 16:12:55 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,48 @@
+$(function(){
+
+//var tagP = $('h1').hide().text('Diamond coring').show(3000);
+
+//$('.nav_links > li').hide(2000).show(1000);
+
+//var w = $('.nav_container').width(600);
+//var h = $('.nav_container').height(250);
+
+//function widthAndHeight(element){
+  //var className="."+element;
+  //var w = $(className).width();
+  //var h = $(className).height();
+  //$('h1').text('Width: '+w+'; Height: '+h);
+//}
+//widthAndHeight('nav_container');
+
+//$('.main_text').html('<p>The link was hear</p>');
+
+//$('h1').fadeOut(2000).fadeIn(3000);
+
+//function elementOut(element, time){
+  //if(time>5000||time<1000||isNaN(time)){
+    //return false;
+  //}
+  //else {
+    //var className = "."+element;
+    //$(className).fadeOut(time);
+  //}
+//}
+//elementOut('nav_container', 3000);
+
+//$('h1').fadeTo(4000, 0.5);
+
+//$('h1').slideUp(2000).slideDown(1000);
+
+//Добавление атрибутов в выражение
+//$('h1').attr('class', 'new_class');
+
+//Добавление и удаление классов
+//$('.nav_container').addClass('border');
+
+//Добавление Css стилей
+//$('h1').css('color', 'blue');
+
+$('.nav_container').animate({'font-size':'20px'}, 3000);
+
+});
